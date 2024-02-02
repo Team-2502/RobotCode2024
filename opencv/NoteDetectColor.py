d885120fa57f36e18d92e087361f5e5d6b38b178
Avery L <64002568@ep-student.org>
2024-02-01 18:56:49 -0600

    notecorrection3 and filtering
    increased contour value and changed hue value

--- opencv/NoteDetectColor.py
+++ opencv/NoteDetectColor.py
@@ -12,10 +12,10 @@ while camera.isOpened():
     cv2.imshow('raw', frame)
 
     # lower range of red color in HSV
-    lower_range = (2, 5, 5)
+    lower_range = (4.5, 5, 5)
     upper_range = (4.9, 255, 255)
-    mask = cv2.inRange(hsv_img, lower_range, upper_range)
-    
+    mask = cv2.inRanggithe(hsv_img, lower_range, upper_range)
+
     lower_range = (5.1, 5, 5)
     upper_range = (30,255,255)
     mask1 = cv2.inRange(hsv_img, lower_range, upper_range)
@@ -33,7 +33,7 @@ while camera.isOpened():
 
     for pic, contour in enumerate (contours):
         area = cv2.contourArea(contour)
-        if (area > 28):
+        if (area > 50):
             x,y,w,h = cv2.boundingRect(contour)
             frame = cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,0),2)
 
