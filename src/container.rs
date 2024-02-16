fce1b147fd76a6cb08aab631d422f0685823b3f4
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-02-15 18:10:32 -0600

    tore out demons

--- src/container.rs
+++ src/container.rs
@@ -12,62 +12,7 @@ pub struct Ferris {
     intake: Rc<RefCell<Intake>>,
     shooter: Rc<RefCell<Shooter>>, 
     climber: Rc<RefCell<Climber>>,
-    intake_state: Rc<RefCell<IntakeState>>,
     grab: Rc<RefCell<Option<JoinHandle<()>>>>,
-}
-
-enum IntakeState { // TODO: use async/await instead of a state machine
-    Not,
-    Accelerating,
-    Running,
-    Stalled,
-}
-
-impl IntakeState {
-    fn running(&self) -> bool {
-        match self {
-            IntakeState::Running | IntakeState::Accelerating => {
-                true
-            }
-            _ => false
-        }
-    }
-
-    fn start(&mut self) {
-        match self {
-            IntakeState::Not => {
-                *self = IntakeState::Accelerating
-            }
-            _ => {}
-        }
-    }
-
-    fn reset(&mut self) {
-        match self {
-            IntakeState::Stalled | IntakeState::Running | IntakeState::Accelerating => {
-                *self = IntakeState::Not
-            }
-            _ => {}
-        }
-    }
-
-    fn spinning(&mut self) {
-        match self {
-            IntakeState::Accelerating => {
-                *self = IntakeState::Running
-            }
-            _ => {}
-        }
-    }
-
-    fn stall(&mut self) {
-        match self {
-            IntakeState::Running => {
-                *self = IntakeState::Stalled
-            }
-            _ => {}
-        }
-    }
 }
 
 impl Ferris {
@@ -76,7 +21,7 @@ impl Ferris {
         let intake = Rc::new(RefCell::new(Intake::new()));
         let shooter = Rc::new(RefCell::new(Shooter::new()));
         let climber = Rc::new(RefCell::new(Climber::new()));
-        Self { drivetrain, intake, shooter, climber, intake_state: Rc::new(RefCell::new(IntakeState::Not)), grab: Rc::new(RefCell::new(None))} 
+        Self { drivetrain, intake, shooter, climber, grab: Rc::new(RefCell::new(None))} 
     }
 }
 
@@ -84,8 +29,6 @@ pub fn container<'a>(left_drive: &mut Joystick, right_drive: &mut Joystick, oper
     let mut drivetrain = robot.drivetrain.deref().borrow_mut();
     let shooter = robot.shooter.deref().borrow();
     let climber = robot.climber.deref().borrow();
-    let mut intake_state = robot.intake_state.deref().borrow_mut();
-
 
     let joystick_range = 0.04..1.;
     let power_translate = if right_drive.get(3) { 0.0..0.3 }
@@ -109,38 +52,23 @@ pub fn container<'a>(left_drive: &mut Joystick, right_drive: &mut Joystick, oper
         drivetrain.reset_angle();
     }
 
-    if operator.get(10) && robot.grab.deref().borrow_mut().is_none() {
+    if operator.get(8) && robot.grab.deref().try_borrow().is_ok_and(|n| n.is_none()) {
         let intake = robot.intake.clone();
         robot.grab.replace(Some(executor.spawn_local(async move {
+            println!("started");
             intake.deref().borrow_mut().grab().await;
         })));
-    } else if !operator.get(10) {
+    } else if !operator.get(8) {
         if let Some(grab) = robot.grab.take() {
-            drop(grab);
+            grab.abort();
         }
     }
-
-    if operator.get(8) {
-        intake_state.start();
-    } else {
-        intake_state.reset();
-    }
-
     
-    if let Ok(mut intake) = robot.intake.try_borrow_mut() {
-        if intake.running() {
-            intake_state.spinning();
-        }
-        if intake.stalled() {
-           intake_state.stall();
-        }
-
-        if intake_state.running() {
+    if let Ok(intake) = robot.intake.try_borrow_mut() {
+        if operator.get(9) {
             intake.set_rollers(0.4);
         } else if operator.get(7) {
             intake.set_rollers(-0.4);
-        } else if operator.get(9) {
-            intake.set_rollers(0.4);
         } else {
             intake.stop_rollers();
         }
